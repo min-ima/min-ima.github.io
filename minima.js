dd69db018d76ea83e928d9272fc7d0f39cff8c8b
min-ima <Irene@dhcp-wlan-uzh-10-12-0-216.uzh.ch>
2016-10-28 15:02:15 +0200

    tile bug

--- minima.js
+++ minima.js
@@ -1,17 +1,17 @@
-var cats = document.getElementsByClassName("tile");
+// var cats = document.getElementsByClassName("tile");
 
-if (true) {
-	console.log("here");
-	var emptyDiv = document.createElement("div")
-	emptyDiv.className="tile";
-	emptyDiv.id="overlay";
-	emptyDiv.style.display="hidden"
-	var emptyDivin = document.createElement("div")
-	emptyDivin.className="tile";
-	emptyDivin.id="category";
-	emptyDivin.style.display="hidden"
-	emptyDivin.style.border="transparent";
-	emptyDiv.appendChild(emptyDivin);
-	var cont =document.body.getElementsByClassName("container")[0];
-	cont.appendChild(emptyDiv)
-}
+// if (true) {
+// 	console.log("here");
+// 	var emptyDiv = document.createElement("div")
+// 	emptyDiv.className="tile empty";
+// 	emptyDiv.id="overlay";
+// 	emptyDiv.style.display="hidden"
+// 	var emptyDivin = document.createElement("div")
+// 	emptyDivin.className="tile empty";
+// 	emptyDivin.id="category";
+// 	emptyDivin.style.display="hidden"
+// 	emptyDivin.style.border="transparent";
+// 	emptyDiv.appendChild(emptyDivin);
+// 	var cont =document.body.getElementsByClassName("container")[0];
+// 	cont.appendChild(emptyDiv)
+// }
